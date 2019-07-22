a169cd593454576c9f84d044ebe05ea39249b0aa
ryan <ryanwkoch@gmail.com>
2019-07-21 21:50:08 -0400

    gulp tasks modified to include css minification, copying all required assets to production/ and a gulp task called clean has been added.
    deploy-to-google.sh is now functional and populates the gcloud bucket
    
    v1 of site is now live

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # open_diagnosis
+
+To deploy to production, run `./deploy-to-google.sh`.
--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,12 @@
-'use strict';
-var gulp = require('gulp');
-var path = require('path');
-var sass = require('gulp-sass');
-var open = require('gulp-open');
-var browserSync = require('browser-sync').create();
-var image = require('gulp-image');
-
-var Paths = {
-  HERE: './',
-  CSS: './production/assets/css/',
-  SCSS_TOOLKIT_SOURCES: './assets/scss/now-ui-kit.scss',
-  SCSS: './production/assets/scss/**/**'
-};
+'use strict'
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var open = require('gulp-open')
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
+var browserSync = require('browser-sync').create()
+var image = require('gulp-image')
+var del = require('del')
 
 // Copy third party libraries from /node_modules into / production
 // gulp.task('production', function () {
@@ -20,54 +15,81 @@ var Paths = {
 //     '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
 //     '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
 //   ]).pipe(gulp.dest('./production/bootstrap'))
-// });
+// })
 
-gulp.task('compile-scss', () => {
-  return gulp.src('./assets/scss/**/*.scss')
+gulp.task('css:compile', (done) => {
+  gulp.src('./assets/scss/**/*.scss')
     .pipe(sass.sync({
       outputStyle: 'expanded'
     }).on('error', sass.logError))
-    .pipe(gulp.dest(Paths.CSS))
-});
+    .pipe(gulp.dest('./assets/css'))
+  done()
+})
 
-gulp.task('image', () => {
+// Minify CSS
+gulp.task('css:minify', (done) => {
+  gulp.src([
+    './assets/css/*.css',
+    '!./assets/css/*.min.css'
+  ])
+    .pipe(cleanCSS())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('./assets/css'))
+  done()
+})
+
+gulp.task('css', gulp.series(['css:compile', 'css:minify']))
+
+gulp.task('image', (done) => {
   gulp.src('./assets/img/*')
     .pipe(image())
-    .pipe(gulp.dest('./production/assets/img'));
-});
+    .pipe(gulp.dest('./production/assets/img'))
+  done()
+})
 
-gulp.task('open', () => {
+gulp.task('open', (done) => {
   gulp.src('index.html')
-    .pipe(open());
-});
+    .pipe(open())
+  done()
+})
 
 // Configure the browserSync task
-gulp.task('browserSync', function () {
-  var pathArray = Object.keys(Paths).map(function (path) {
-    return Paths[path];
-  });
+gulp.task('browserSync', (done) => {
+  var paths = ['./', './**/**.html', './assets/scss/**/**', './assets/js/*.js']
 
-  browserSync.init(pathArray, {
+  browserSync.init(paths, {
     server: {
       baseDir: './'
     }
-  });
-});
+  })
+
+  done()
+})
 
 
 
-gulp.task('dev', gulp.parallel(['browserSync', 'compile-scss', 'open']), () => {
-  gulp.watch('./assets/scss/**/*.sccs', ['compile-scss']);
-  gulp.watch('./assets/js/*.js', ['js']);
-  gulp.watch('./*.html', browserSync.reload);
-});
+gulp.task('dev', gulp.parallel(['browserSync', 'css', 'open']), (done) => {
+  gulp.watch('./assets/scss/**/*.sccs', ['compile-scss'])
+  gulp.watch('./assets/js/*.js', ['js'])
+  gulp.watch('./*.html', browserSync.reload)
+  done()
+})
+
+gulp.task('clean', (done) => {
+  del.sync(['production/**'])
+  done()
+})
 
 // Copies assets needed for production into production folder
-gulp.task('copy', () => {
-  gulp.src('./index.html').pipe(gulp.dest('./production/'));
-  gulp.src('./assets/fonts/*').pipe(gulp.dest('./production/assets/fonts/'));
-  gulp.src('./nucleo-icons.html').pipe(gulp.dest('./production/'));
-  gulp.src('./coming-soon.html').pipe(gulp.dest('./production/'));
-});
+gulp.task('copy', (done) => {
+  gulp.src('./assets/fonts/*').pipe(gulp.dest('./production/assets/fonts/'))
+  gulp.src('./assets/js/**/*.js').pipe(gulp.dest('./production/assets/js/'))
+  gulp.src('./assets/css/**/*.css').pipe(gulp.dest('./production/assets/css/'))
+  gulp.src('./index.html').pipe(gulp.dest('./production/'))
+  gulp.src('./*.html').pipe(gulp.dest('./production/'))
+  done()
+})
 
-gulp.task('production', gulp.series(['compile-scss', 'image', 'copy']));
+gulp.task('production', gulp.series(['clean', 'css', 'image', 'copy']))
